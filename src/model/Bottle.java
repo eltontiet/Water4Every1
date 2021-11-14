0c1f205cc5d885c973fcb81544d1bf5a259dd00d
Melissa <medlin@students.cs.ubc.ca>
2021-11-14 11:42:23 -0800

    tested and implemented bottle class

--- src/model/Bottle.java
+++ src/model/Bottle.java
@@ -1,4 +1,46 @@
 package model;
 
 public class Bottle {
+    // how much water can be stored in the bottle (mL)
+    private int capacity;
+
+    // how much water is currently in the bottle (mL)
+    // INVARIANT: 0 <= water <= capacity
+    private int waterLevel;
+
+
+    //EFFECTS: constructs a full water bottle
+    public Bottle(int capacity) {
+        this.capacity = capacity;
+        this.waterLevel = capacity;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public int getWaterLevel() {
+        return waterLevel;
+    }
+
+    //EFFECTS: modifies current water bottle capacity
+    public void changeCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    //EFFECTS: changes current water level
+    public void drinkWater(int drank) {
+        int water = drank%capacity;
+
+        int currentLevel = waterLevel - water;
+
+        if (currentLevel > 0) {
+            waterLevel = currentLevel;
+        } else {
+            currentLevel = water - waterLevel;
+            waterLevel = capacity - currentLevel;
+        }
+
+    }
+
 }
---
+++ tests/BottleTest.java
@@ -0,0 +1,56 @@
+package tests;
+
+import model.Bottle;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BottleTest {
+    Bottle bottle;
+
+    @BeforeEach
+    public void setup() {
+        bottle = new Bottle(1000);
+    }
+
+    @Test
+    public void changeCapacity() {
+        bottle.changeCapacity(1500);
+        assertEquals(1500, bottle.getCapacity());
+    }
+
+    @Test
+    public void drinkLittle() {
+        bottle.drinkWater(200);
+        assertEquals(800, bottle.getWaterLevel());
+    }
+
+    @Test
+    public void drinkLittles() {
+        bottle.drinkWater(200);
+        bottle.drinkWater(500);
+        assertEquals(300, bottle.getWaterLevel());
+    }
+
+    @Test
+    public void drinkLittleOverflow() {
+        bottle.drinkWater(300);
+        bottle.drinkWater(500);
+        bottle.drinkWater(500);
+        assertEquals(700, bottle.getWaterLevel());
+    }
+
+    @Test
+    public void drinkLot() {
+        bottle.drinkWater(1100);
+        assertEquals(900, bottle.getWaterLevel());
+    }
+
+    @Test
+    public void drinkLots() {
+        bottle.drinkWater(1100);
+        bottle.drinkWater(2300);
+        assertEquals(600, bottle.getWaterLevel());
+    }
+}
