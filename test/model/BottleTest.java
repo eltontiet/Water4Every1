ea25740b645611ef69726295653fc14ecd4a9ec9
Elton <eltontiet@gmail.com>
2021-11-14 13:04:13 -0800

    Move tests into the same folder

--- test/model/BottleTest.java
+++ test/model/BottleTest.java
@@ -1,10 +1,9 @@
-package tests;
+package model;
 
-import model.Bottle;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BottleTest {
     Bottle bottle;
--- test/model/UserTest.java
+++ test/model/UserTest.java
@@ -1,8 +1,9 @@
-import model.User;
+package model;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UserTest {
     User user;
