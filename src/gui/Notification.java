ccea09bf4aa6caf953337289e0549b0929245809
mn2002 <mxnguyyen@gmail.com>
2021-11-14 18:52:58 -0800

    Implemented small sized notification pop up that reminds user to drink water in gui's Notification.

--- src/gui/Notification.java
+++ src/gui/Notification.java
@@ -1,4 +1,44 @@
 package gui;
 
-public class Notification {
+import javax.swing.*;
+import java.awt.*;
+
+public class Notification extends JFrame {
+    public static final int WIDTH = 350;
+    public static final int HEIGHT = 75;
+    private static final Font FONT_LARGE = new Font(null, Font.PLAIN, 25);
+
+    private JPanel notificationPanel;
+
+    //EFFECTS: constructs a small notification pop up that reminds user to drink water
+    public Notification() {
+        super("Notification");
+        initializeGraphics();
+    }
+
+    //test
+    public static void main(String[] args) {
+        new Notification();
+    }
+    private void initializeGraphics() {
+        setLayout(new BorderLayout());
+        setMinimumSize(new Dimension(WIDTH, HEIGHT));
+        displayNotificationPanel();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    private void displayNotificationPanel() {
+        notificationPanel = new JPanel();
+        JLabel notificationText1 = new JLabel("REMINDER:" + "\nDRINK WATER");
+        notificationText1.setFont(FONT_LARGE);
+        notificationPanel.add(notificationText1);
+        notificationPanel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        notificationPanel.setLocation(0, HEIGHT/2);
+        notificationPanel.setVisible(true);
+        add(notificationPanel, BorderLayout.CENTER);
+    }
+
+
 }
